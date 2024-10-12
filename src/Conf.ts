93fe76c407c1b30da0176deabed3b3d7230b1780
octest <octest8848@qq.com>
2024-10-13 07:17:57 +0800

    01

--- src/Conf.ts
+++ src/Conf.ts
@@ -1,4 +1,4 @@
-// export const serverAddress = "http://127.0.0.1:8080/"
-export const serverAddress = "https://fictional-succotash-pjjv4xg9qp54hrqwx-8080.app.github.dev/"
+export const serverAddress = "http://127.0.0.1:8080/"
+// export const serverAddress = "https://fictional-succotash-pjjv4xg9qp54hrqwx-8080.app.github.dev/"
 // 直接在变量后面加资源相对路径就好了，如image.png
 export const serverStaticAssetsAddress = serverAddress + "static/"
